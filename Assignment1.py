540dd512a9c5c895abbb3a7c6d5016fe74563f27
axeld27 <axeldomguia@gmail.com>
2025-02-27 20:38:08 +0000

    final

--- Assignment1.py
+++ Assignment1.py
@@ -38,11 +38,20 @@ def star_shape(rows):
 # Function 5: While Loop – Counting Multiples of 3
 # This function should return a list of numbers from 1 to limit, replacing multiples of 3 with "Multiple of 3".
 def count_multiples_of_3(limit):
-    # TODO: Implement this function
-    pass  # Replace with your code
+    result = ""
+    for number in range(1, limit + 1):
+        if number % 3 == 0:
+            result += "Multiple of 3\n"
+        else:
+            result += str(number) + "\n"
+    return result.strip()
 
 # Function 6: Sum of Even Numbers in a Range
 # This function should calculate and return the sum of even numbers within a given range.
 def sum_of_even_numbers(start, end):
-    # TODO: Implement this function
-    pass  # Replace with your code
+    number = []
+    for i in range (start, end+ 1):
+        if i % 2 ==0:
+            number.append(i)
+        result = sum(number) 
+    return result
